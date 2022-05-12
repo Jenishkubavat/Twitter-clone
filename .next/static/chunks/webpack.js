211e445c53f930f36a00a3dac17c10935a925558
jenishkubavat <dante2709201@gmail.com>
2022-05-12 13:02:35 +0530

    enviroment update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "762c52db127f30c3"; }
+/******/ 		__webpack_require__.h = function() { return "a0b879cfd6d9064c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
